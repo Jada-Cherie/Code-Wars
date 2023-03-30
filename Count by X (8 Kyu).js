0a2617984f93305df20da1d15cff608d36b24921
Jada-Cherie <jada.c.townsend@gmail.com>
2023-03-30 11:18:58 -0400

    first push

---
+++ Count by X (8 Kyu).js
@@ -0,0 +1,25 @@
+// Count by X (8 Kyu)
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+
+// Examples
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+
+//created function with two arguement
+function countBy(x, n) {
+    //create a new array
+    let z = [];
+    //created a for loop
+    for(let i = 1; i <= n; i++){
+    //attached method to array, the elements in the parenthesis will add to the end of the array.
+      z.push(i * x)
+    } 
+    // return the result
+    return z;
+  }
