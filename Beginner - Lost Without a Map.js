354a241c9e4dc11e9a9fc35b47f678644d790afa
Jada-Cherie <jada.c.townsend@gmail.com>
2023-04-08 14:21:19 -0400

    first push

---
+++ Beginner - Lost Without a Map.js
@@ -0,0 +1,12 @@
+//Beginner - Lost Without a Map(8kyu)
+
+//https://www.codewars.com/kata/57f781872e3d8ca2a000007e/javascript
+
+//Instructions
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+    return x.map(num => num * 2)
+    }
