be3ca1ad32b48503d06d21892bfe3597ffaa51bc
Jada-Cherie <jada.c.townsend@gmail.com>
2023-05-25 18:10:35 -0400

    update practice

--- Function 1 - hello world.js
+++ Function 1 - hello world.js
@@ -4,3 +4,39 @@ function greet(){
     return greet
   }
   greet()
+
+//Practice//
+function greet () {
+  let greet = "Hello World"
+  return greet
+}
+greet()
+// Write a function "greet" that returns "hello world!"
+function greet (){
+  let greet = "Hello World"
+  return greet
+}
+greet ()
+// Write a function "greet" that returns "hello world!"
+
+function greet (){
+  let greet = "Hello World"
+  return greet
+}
+greet()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Square(n) Sum (8kyu).js
+++ Square(n) Sum (8kyu).js
@@ -13,5 +13,35 @@
 function squareSum(numbers){
     let sum = 0
     numbers.forEach(number => sum += number ** 2)
+    //forEach is a method that goes through each slot in the array 
     return sum
   }
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 +2^2 = 9
+
+//square each of the numbers in the array
+//take all of the numbers and then add them together
+function SqSum (numbers) {
+  let sum = 0
+  numbers.forEach(number => sum += number ** 2)
+
+  return sum
+} 
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
