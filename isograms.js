bd69b6b2c3f005f08745409eeaf0c8a7ca441e85
Jada-Cherie <jada.c.townsend@gmail.com>
2023-07-12 09:39:32 -0400

    commit

--- isograms.js
+++ isograms.js
@@ -14,3 +14,5 @@ function isIsogram(str){
     }
     return true;
  }
+
+ 
---
+++ sumMixedArray.js
@@ -0,0 +1,5 @@
+//Sum Mixed Array
+
+//https://www.codewars.com/kata/57eaeb9578748ff92a000009/javascript
+
+sumMix = x => x.reduce((prev,curr) => +curr + prev, 0)
